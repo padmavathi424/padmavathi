2b0445d904e764c99ed0df09e33bd35a0c4ea321
padmavathi424 <41770642+padmavathi424@users.noreply.github.com>
2018-08-27 11:32:07 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,8 +1,8 @@
-a=5
-if(a>0):
-print('positive')
-elif(a==0):
-print('zero')
+num=float(input("Enter a number"))
+if num>0:
+     print('Positive')
+elif num==0:
+      print('Zero')
 else:
-print('negative')
+      print('Negative')
 
