f6e6a0b88d8647acaa7a4adbe2f99cc76492273c
padmavathi424 <41770642+padmavathi424@users.noreply.github.com>
2018-08-30 15:34:15 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,10 @@
+m=int(input('Enter num'))
+n=int(input('Enter num'))
+o=int(input('Enter num'))
+if(m>n):
+if(m>o):
+print('m is greater')
+elif:
+print('o is greater')
+else:
+print('n is greater')
