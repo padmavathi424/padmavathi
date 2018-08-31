760a8d7223ca95bb20da1a1048355a3fef3d9027
padmavathi424 <41770642+padmavathi424@users.noreply.github.com>
2018-08-31 12:02:02 +0530

    Update natural.py

--- natural.py
+++ natural.py
@@ -1,14 +1,3 @@
-print("Enter'0' for exit."0;
-num=int(input())
-if num==0:
-exit();
-elif num<1:
-print("Enter a positive number")
-else:
-sum=0;
-while num>0:
-sum+=num;
-num==1;
-print("sum=",sum);
-      
-      
+N=int(input())
+sum=(N*(N=1/2))
+print(p)
